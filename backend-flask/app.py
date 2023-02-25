f844a5f3f2bca8b1fde9f2a6da190c6b4dca20a4
Bryan Kiseu <bryantskisseu@gmail.com>
2023-02-25 08:22:12 +0000

    backend: create notification endpoint

--- backend-flask/app.py
+++ backend-flask/app.py
@@ -4,6 +4,7 @@ from flask_cors import CORS, cross_origin
 import os
 
 from services.home_activities import *
+from services.notification_activities import *
 from services.user_activities import *
 from services.create_activity import *
 from services.create_reply import *
@@ -60,6 +61,12 @@ def data_create_message():
     return model['data'], 200
   return
 
+@app.route("/api/activities/notifications", methods=['GET'])
+@cross_origin()
+def data_notification():
+  data = NotificationActivities.run()
+  return data, 200
+
 @app.route("/api/activities/home", methods=['GET'])
 def data_home():
   data = HomeActivities.run()
@@ -98,7 +105,7 @@ def data_activities():
 
 @app.route("/api/activities/<string:activity_uuid>", methods=['GET'])
 def data_show_activity(activity_uuid):
-  data = ShowActivity.run(activity_uuid=activity_uuid)
+  data = ShowActivities.run(activity_uuid=activity_uuid)
   return data, 200
 
 @app.route("/api/activities/<string:activity_uuid>/reply", methods=['POST','OPTIONS'])
--- backend-flask/services/notification_activities.py
+++ backend-flask/services/notification_activities.py
@@ -1,11 +1,11 @@
 from datetime import datetime, timedelta, timezone
-class HomeActivities:
+class NotificationActivities:
   def run():
     now = datetime.now(timezone.utc).astimezone()
     results = [{
       'uuid': '68f126b0-1ceb-4a33-88be-d90fa7109eee',
-      'handle':  'Andrew Brownie',
-      'message': 'Cloud is fun!',
+      'handle':  'Bryant Kiseu',
+      'message': 'I love my life!',
       'created_at': (now - timedelta(days=2)).isoformat(),
       'expires_at': (now + timedelta(days=5)).isoformat(),
       'likes_count': 5,
@@ -24,8 +24,8 @@ class HomeActivities:
     },
     {
       'uuid': '66e12864-8c26-4c3a-9658-95a10f8fea67',
-      'handle':  'Worf',
-      'message': 'I am out of prune juice',
+      'handle':  'Twinkle light',
+      'message': 'I am out of coconut oil',
       'created_at': (now - timedelta(days=7)).isoformat(),
       'expires_at': (now + timedelta(days=9)).isoformat(),
       'likes': 0,
@@ -33,8 +33,8 @@ class HomeActivities:
     },
     {
       'uuid': '248959df-3079-4947-b847-9e0892d1bab4',
-      'handle':  'Garek',
-      'message': 'My dear doctor, I am just simple tailor',
+      'handle':  'Eunice',
+      'message': 'Babe send me money',
       'created_at': (now - timedelta(hours=1)).isoformat(),
       'expires_at': (now + timedelta(hours=12)).isoformat(),
       'likes': 0,
